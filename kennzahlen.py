7ae588de18120084ea11292de93f940ce68edcb7
Jonas Kubelke <jonas.kubelke@gmail.com>
2023-09-15 21:48:56 +0200

    edit

--- kennzahlen.py
+++ kennzahlen.py
@@ -1,16 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# Ausgabe der Kennzahlen
-def print_stats(merkmal_of_interest, stats_to_print):
-    print('\n' + merkmal_of_interest)
-    print('Modalwert:\t' + str(stats_to_print[0]))
-    print('Median:\t\t' + str(stats_to_print[1]))
-    print('Mittelwert:\t' + str(stats_to_print[2]))
-    print('0.25-Quartil\t' + str(stats_to_print[3]))
-    print('0.75-Quartil\t' + str(stats_to_print[4]))
-    print('Varianz\t\t' + str(stats_to_print[5]))
-    print('Standardabw.\t' + str(stats_to_print[6]))
 
 # Methode zur Berechnung des Modalwertes
 def mod(a):
@@ -18,52 +8,50 @@ def mod(a):
     ind = np.argmax(counts)
     return values[ind]
 
+
+def get_stats(datapoints):
+    stats_of_interest = []
+
+    stats_of_interest.insert(0, mod(datapoints))
+    stats_of_interest.insert(1, np.median(datapoints))
+    stats_of_interest.insert(2, np.round(np.mean(datapoints), decimals=1))
+    stats_of_interest.insert(3, np.quantile(datapoints, 0.25))
+    stats_of_interest.insert(4, np.quantile(datapoints, 0.75))
+    stats_of_interest.insert(5, np.min(datapoints))
+    stats_of_interest.insert(6, np.max(datapoints))
+    stats_of_interest.insert(7, np.round(np.var(datapoints), decimals=2))
+    stats_of_interest.insert(8, np.round(np.std(datapoints),decimals=2))
+
+    return stats_of_interest
+
+
 # Excel-Datei mit Beisspieldatensatz in ein Dataframe einlesen
 df_raw = pd.read_excel('beispieldatensatz.xlsx')
 
-# Kennzahlen ausgeben
-stats = []
+# Je ein Dataframe für m und eines für w erzeugen
+df_m = df_raw.loc[df_raw['Geschlecht'] == 'm']
+df_w = df_raw.loc[df_raw['Geschlecht'] == 'w']
 
-## Geschlecht
-merkmal = 'Geschlecht'
-stats.insert(0, mod(df_raw[merkmal]))
-stats.insert(1, '-')
-stats.insert(2, '-')
-stats.insert(3, '-')
-stats.insert(4, '-')
-stats.insert(5, '-')
-stats.insert(6, '-')
-print_stats(merkmal, stats)
+# Kennzahlen ermitteln
+kennzahlen = ['mod', 'med', 'x̄', 'q1', 'q3', 'min', 'max', 'var', 'sd']
+merkmale = ['Körpergröße', 'Körpergewicht', 'Note']
 
-## Körpergröße
-merkmal = 'Körpergröße'
-stats.insert(0, '-')
-stats.insert(1, np.median(df_raw[merkmal]))
-stats.insert(2, np.round(np.mean(df_raw[merkmal]),decimals=1))
-stats.insert(3, np.quantile(df_raw[merkmal], 0.25))
-stats.insert(4, np.quantile(df_raw[merkmal], 0.75))
-stats.insert(5, np.round(np.var(df_raw[merkmal]),decimals=2))
-stats.insert(6, np.round(np.std(df_raw[merkmal]),decimals=2))
-print_stats(merkmal, stats)
+df_kennzahlen = pd.DataFrame(index=kennzahlen)
+get_stats(datapoints=df_raw[merkmale[0]])
 
-## Körpergewicht
-merkmal = 'Körpergewicht'
-stats.insert(0, '-')
-stats.insert(1, np.median(df_raw[merkmal]))
-stats.insert(2, np.round(np.mean(df_raw[merkmal]),decimals=1))
-stats.insert(3, np.quantile(df_raw[merkmal], 0.25))
-stats.insert(4, np.quantile(df_raw[merkmal], 0.75))
-stats.insert(5, np.round(np.var(df_raw[merkmal]),decimals=2))
-stats.insert(6, np.round(np.std(df_raw[merkmal]),decimals=2))
-print_stats(merkmal, stats)
+# Körpergröße
+df_kennzahlen.insert(0, "Körpergröße", get_stats(datapoints=df_raw[merkmale[0]]), True)
+df_kennzahlen.insert(1, "Körpergröße (m)", get_stats(datapoints=df_m[merkmale[0]]), True)
+df_kennzahlen.insert(2, "Körpergröße (w)", get_stats(datapoints=df_w[merkmale[0]]), True)
 
-## Note
-merkmal = 'Note'
-stats.insert(0, mod(df_raw[merkmal]))
-stats.insert(1, np.median(df_raw[merkmal]))
-stats.insert(2, np.round(np.mean(df_raw[merkmal]),decimals=1))
-stats.insert(3, np.quantile(df_raw[merkmal], 0.25))
-stats.insert(4, np.quantile(df_raw[merkmal], 0.75))
-stats.insert(5, np.round(np.var(df_raw[merkmal]),decimals=2))
-stats.insert(6, np.round(np.std(df_raw[merkmal]),decimals=2))
-print_stats(merkmal, stats)
+# Körpergewicht
+df_kennzahlen.insert(3, "Körpergewicht", get_stats(datapoints=df_raw[merkmale[1]]), True)
+df_kennzahlen.insert(4, "Körpergewicht (m)", get_stats(datapoints=df_m[merkmale[1]]), True)
+df_kennzahlen.insert(5, "Körpergewicht (w)", get_stats(datapoints=df_w[merkmale[1]]), True)
+
+# Note
+df_kennzahlen.insert(6, "Note", get_stats(datapoints=df_raw[merkmale[2]]), True)
+df_kennzahlen.insert(7, "Note (m)", get_stats(datapoints=df_m[merkmale[2]]), True)
+df_kennzahlen.insert(8, "Note (w)", get_stats(datapoints=df_w[merkmale[2]]), True)
+
+df_kennzahlen.to_excel('kennzahlen.xlsx')
